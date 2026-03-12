5ed8f8bd119e8557cf23d6e094267de1f507ea7e
venumadhava123348 <manyamvenumadhavareddy@farviewglobal.com>
2026-03-12 13:25:15 +0530

    Added LeetCode Java solutions

---
+++ Longestsubstring.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Longestsubstring {
+    public static void main(String[] args) {
+        
+    
+    String s="aaacccccc";
+    int left=0;
+    int max=0;
+    HashSet<Character> set=new HashSet<>();
+    for(int i=0;i<s.length();i++){
+           while(set.contains(s.charAt(i))){
+            set.remove(s.charAt(left));
+            left++;
+           }
+           set.add(s.charAt(i));
+           max=Math.max(max,i-left+1);
+    }
+    System.out.println(max);
+}
+}
