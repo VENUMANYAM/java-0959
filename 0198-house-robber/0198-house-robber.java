cade3facf281990c3ba30f36ab8624bf2371b8f2
MADHAV REDDY <161930462+VENUMANYAM@users.noreply.github.com>
2026-03-17 10:23:28 +0530

    Time: 0 ms (100%), Space: 42.7 MB (84.88%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0)return 0;
+        if(nums.length==1)return nums[0];
+
+       int prev1=0;
+       int prev2=0;
+       for(int i=0;i<nums.length;i++){
+        int num=nums[i];
+        int temp=Math.max(prev1,prev2+num);
+        prev2=prev1;
+        prev1=temp;
+       }
+       return prev1;
+    }
+}
